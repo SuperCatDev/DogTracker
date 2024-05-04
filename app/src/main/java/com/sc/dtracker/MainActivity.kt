2d83f201135697abb855b6648ac366f8f495127e
Nikita Timchenko <n.timchenko@vk.team>
2024-05-04 19:54:02 +0300

    Added initial map integration

--- app/src/main/java/com/sc/dtracker/MainActivity.kt
+++ app/src/main/java/com/sc/dtracker/MainActivity.kt
@@ -8,12 +8,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.sc.dtracker.features.map.ui.MapViewContainer
+import com.sc.dtracker.features.map.ui.MapViewHost
 import com.sc.dtracker.ui.BaseScreen
+import com.sc.dtracker.ui.ext.lazyUnsafe
 import com.sc.dtracker.ui.theme.DogTrackerTheme
+import com.yandex.mapkit.MapKitFactory
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), MapViewHost {
+
+    private val mapViewContainer by lazyUnsafe {
+        MapViewContainer(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MapKitFactory.initialize(this)
         enableEdgeToEdge()
         setContent {
             DogTrackerTheme(dynamicColor = false) {
@@ -25,5 +35,22 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        mapViewContainer.onCreate()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        mapViewContainer.onStart()
+    }
+
+    override fun onStop() {
+        MapKitFactory.getInstance().onStop()
+        mapViewContainer.onStop()
+        super.onStop()
+    }
+
+    override fun provideMapViewContainer(): MapViewContainer {
+        return mapViewContainer
     }
 }
